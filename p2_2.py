c90cef8dd763d2df37da399f5a0a7247b7fcdd92
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-09 13:01:56 -0400

    initial commit

---
+++ p2_2.py
@@ -0,0 +1,14 @@
+
+food_1 = input("what is your second most favorite food?")
+
+food_2 = input("what is your favorite food?")
+
+print("would you eat",end = " ")
+print(food_1, food_2, sep='')
+
+
+
+
+
+
+
