642f2e733d8d9d2da99c4c43259f318e5385600a
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-10 18:08:34 -0400

    small things

--- p3_3.py
+++ p3_3.py
@@ -7,7 +7,7 @@ import random
 guess = int(input("\t\tHuman... \n\t\tGuess my number between 1 and 100.\
 		\n\t\tYou will have only 5 attempts. I play for keeps..."))
 
-num = random.randint(1,10)
+num = random.randint(1,100)
 
 
 #counter and chat for first 4 guesses
@@ -18,13 +18,10 @@ while True:
     print("\t\tYOU WIN! You are wiser then expected ...\n\t\t\
            human . . .")
     break
-
   elif guess > num:
     guess = int(input("\n\t\tToo high, Guess again"))
-
   elif guess < num:
     guess = int(input("\n\t\tToo low, guess again"))
-
   if count == 4:
     print("\n\t\tYou have only one guess left human, \n\n\
            \tchoose wisely . . .")
@@ -32,21 +29,17 @@ while True:
     break
 
 
-
-#counter fnd chat for final guess
+#counter and chat for final guess
 while guess != num:
   count += 1
   if guess == num:
     print("\t\tYOU WIN, You are wiser then expected ...\n\t\t\
            human . . .")
     break
-
   elif guess > num:
     print("\n\t\tToo high")
-
   elif guess < num:
     print("\n\t\tToo low")
-  
   if count == 5:
-    print("\t\t\nHA! THE NUMBER WAS", str(num) + ".", "I GAIN MORE POWERFUL WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
+    print("\t\t\nHA! THE NUMBER WAS", str(num) + ".", "I GAIN MORE POWER WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
     break
