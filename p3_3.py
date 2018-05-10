7e8624beac16458bc4703dfce88139b9b2d40b02
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-09 20:47:48 -0400

    chapter 3, problem 3

---
+++ p3_3.py
@@ -0,0 +1,34 @@
+#create a guess the number game. number between 1-100. limited attempts
+
+import random
+
+guess = int(input("\t\tHuman... \n\t\tGuess my number between 1 and 100.\
+		\n\t\tYou will have only 5 attempts. I play for keeps..."))
+
+num = random.randint(1,100)
+
+
+
+count = 0
+while True: 
+
+  if guess == num:
+    print("\t\tYou are wiser then expected ...\n\t\t\
+           human . . .")
+
+  elif guess > num:
+    count += 1
+    guess = int(input("\n\t\tToo high, Guess again"))
+
+  elif guess < num:
+    count += 1
+    guess = int(input("\n\t\tToo low, guess again"))
+
+ 
+  if count == 4:
+    print("\n\t\tYou have only one guess left human, \n\n\
+           \tI knew you were weak minded")
+
+  if count == 5:
+    print("\t\t\nI GAIN MORE POWERFUL WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
+    break
