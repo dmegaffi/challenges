2b0447b423b4dc4c9e54568e53e6dcb58b67f7cc
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-10 07:24:41 -0400

    added chat and new loop for last guess

--- p3_3.py
+++ p3_3.py
@@ -10,25 +10,41 @@ guess = int(input("\t\tHuman... \n\t\tGuess my number between 1 and 100.\
 num = random.randint(1,100)
 
 
-
-count = 0
+#counter and chat for first 4 guesses
+count = 1 
 while True: 
+  count += 1
   if guess == num:
     print("\t\tYou are wiser then expected ...\n\t\t\
            human . . .")
-  
+    break
+
   elif guess > num:
-    count += 1
     guess = int(input("\n\t\tToo high, Guess again"))
 
   elif guess < num:
-    count += 1
     guess = int(input("\n\t\tToo low, guess again"))
 
   if count == 4:
     print("\n\t\tYou have only one guess left human, \n\n\
-           \tI knew you were weak minded")
+           \tchoose wisely . . .")
+    break
 
+guess = int(input("\n\t\twhat is your final guess?"))
+
+#counter fnd chat for final guess
+while True:
+  count += 1
+  if guess == num:
+    print("\t\tYou are wiser then expected ...\n\t\t\
+           human . . .")
+
+  elif guess > num:
+    print("\n\t\tToo high")
+
+  elif guess < num:
+    print("\n\t\tToo low")
+  
   if count == 5:
     print("\t\t\nHA! THE NUMBER WAS", str(num) + ".", "I GAIN MORE POWERFUL WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
     break
