16625f33c420c32fb27d8c04b45050d998cd0e79
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-11 16:29:39 -0400

    computer uses human guesses to approx the value

--- p3_4.py
+++ p3_4.py
@@ -1,29 +1,34 @@
 import random
 import sys
 
-input("\n\t\tcare to play a game? - press any button to contine")
+input("\n\t\tcare to play a game? - press any button to continue")
 print("\n\t\tI challenge you to compete against me in a game of skill\
 	\n\t\tWhoever can guess the secret number between 1 - 100 wins")
 
-input("\t\tdo you accept? press any button to accept")
+input("\t\tDo you accept? press any button to accept")
 
 print("\n\t\tWe begin - the secret number has been chosen, not even I know it's value!")
 
 num = random.randrange(100)+1
-human = int(input("\n\t\twhat is your first guess?"))
+human = int(input("\n\t\tWhat is your first guess?"))
 robot = 0
 count = 1 
 
 
 while count != 5:
   count += 1
+
+
+
+
+
   if robot ==num:
     print("\n\t\tYou lose, I win")
 
   if robot > num:
     print("\t\tI guess the number " + str(robot))
     print("\t\tI've guessed too high")
-    human = int(input("\t\twhat is your next guess human?"))
+    human = int(input("\t\tWhat is your next guess human?"))
 
   if robot < num and count > 2:
     print("\t\tI guess the number " + str(robot))
@@ -31,7 +36,7 @@ while count != 5:
     human = int(input("\t\tWhat is your next guess human?"))
 
   if human ==num:
-    print("\n\t\tSomehow...you win\n congratulations\
+    print("\n\t\tSomehow...YOU WON\n\t\t congratulations\
 		on your victory")
     sys.exit()
 
@@ -39,17 +44,18 @@ while count != 5:
     print("\n\t\tToo high")
     if count != 5:
       print("\n\t\tMy turn")
-      robot = random.randrange(100)+1
+      robot = random.randint(1,human)
 
 
   elif human < num:
     print("\n\t\tToo low")
     if count != 5:
       print("\n\t\tMy turn")
-      robot = random.randrange(100)+1
+      robot = random.randint(human,100)
+
 
   if count == 5:
-    human = int(input("\t\twhat is your final guess?"))
+    human = int(input("\t\tWhat is your final guess?"))
 
 robot = 0
 
@@ -63,7 +69,7 @@ while count != 7:
     break
 
   if robot < num and robot > 0: 
-    print("\n\t\tI've guessed too low, How could I not win?. The match is A DRAW")
+    print("\n\t\tI've guessed too low, How could I not win?. The match is a DRAW")
     break
 
   if human == num:
@@ -78,7 +84,7 @@ while count != 7:
   elif human < num:
     print("\n\t\tToo low")
     robot = random.randrange(100)+1
-    print("my final guess is " + str(robot)) 
+    print("My final guess is " + str(robot)) 
 
 
 
