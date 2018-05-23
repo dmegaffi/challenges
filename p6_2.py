e79415158a29551c876857b5ef299d0949463819
Dennis Megaffin <dennismegaffin@gmail.com>
2018-05-23 12:36:06 -0400

    added a main function

--- p6_2.py
+++ p6_2.py
@@ -1,5 +1,5 @@
 #use the ask_number function to alter a previous challenge from chapter 3
-
+#created main function but have not yet covered "if name == main"
 import random
 
 
@@ -11,42 +11,44 @@ def ask_number(question, low, high, step = 1):
     return response
 
 
+def main():
+  guess = ask_number("Guess my number betweeen 1 and 100", 1, 100)
 
-guess = ask_number("Guess my number betweeen 1 and 100", 1, 100)
-
-num = random.randint(1,100)
+  num = random.randint(1,100)
 
 
 #counter and chat for first 4 guesses
-count = 1 
-while True: 
-  count += 1
-  if guess == num:
-    print("\t\tYOU WIN! You are wiser then expected ...\n\t\t\
-           human . . .")
-    break
-  elif guess > num:
-    guess = ask_number("\n\t\tToo high, Guess again",1,100)
-  elif guess < num:
-    guess = ask_number("\n\t\tToo low, guess again",1,100)
-  if count == 4:
-    print("\n\t\tYou have only one guess left human, \n\n\
-           \tchoose wisely . . .")
-    guess = ask_number("\n\t\twhat is your final guess?",1,100)
-    break
+  count = 1 
+  while True: 
+    count += 1
+    if guess == num:
+      print("\t\tYOU WIN! You are wiser then expected ...\n\t\t\
+             human . . .")
+      break
+    elif guess > num:
+      guess = ask_number("\n\t\tToo high, Guess again",1,100)
+    elif guess < num:
+      guess = ask_number("\n\t\tToo low, guess again",1,100)
+    if count == 4:
+      print("\n\t\tYou have only one guess left human, \n\n\
+             \tchoose wisely . . .")
+      guess = ask_number("\n\t\twhat is your final guess?",1,100)
+      break
 
 
 #counter and chat for final guess
-while guess != num:
-  count += 1
-  if guess == num:
-    print("\t\tYOU WIN, You are wiser then expected ...\n\t\t\
-           human . . .")
-    break
-  elif guess > num:
-    print("\n\t\tToo high")
-  elif guess < num:
-    print("\n\t\tToo low")
-  if count == 5:
-    print("\t\t\nHA! THE NUMBER WAS", str(num) + ".", "I GAIN MORE POWER WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
-    break
+  while guess != num:
+    count += 1
+    if guess == num:
+      print("\t\tYOU WIN, You are wiser then expected ...\n\t\t\
+             human . . .")
+      break
+    elif guess > num:
+      print("\n\t\tToo high")
+    elif guess < num:
+      print("\n\t\tToo low")
+    if count == 5:
+      print("\t\t\nHA! THE NUMBER WAS", str(num) + ".", "I GAIN MORE POWER WITH EVERY DEFEAT OF THE WEAK MINDED HUMAN")
+      break
+
+main()
